8ca56f3a5220effc46588e0a2be59191406edc50
cjaybond <jamesuchennachi@gmail.com>
2023-01-17 22:59:11 +0100

    Added pagination and sorting

--- eduSync-admin-portal-ui/src/app/students/students.component.ts
+++ eduSync-admin-portal-ui/src/app/students/students.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
 
 @Component({
@@ -7,6 +11,12 @@ import { StudentService } from './student.service';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
+  
+  students: Student[] = [];
+  displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile', 'gender'];
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
 
   constructor(private studentService: StudentService) { }
 
@@ -14,7 +24,16 @@ export class StudentsComponent implements OnInit {
     this.studentService.getStudent()
     .subscribe(
       (successResponse) => {
-        console.log(successResponse);
+        this.students = successResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
+
+        if(this.matPaginator){
+          this.dataSource.paginator = this.matPaginator;
+        }
+
+        if(this.matSort){
+          this.dataSource.sort = this.matSort;
+        }
       },
       (errorResponse) => {
         console.log(errorResponse);
