6f103d218e50723976eecfeb5d8d88756b63fb58
cjaybond <jamesuchennachi@gmail.com>
2023-01-18 11:39:50 +0100

    Fixed gender list bug

--- eduSync-admin-portal-ui/src/app/services/gender.service.ts
+++ eduSync-admin-portal-ui/src/app/services/gender.service.ts
@@ -13,6 +13,6 @@ export class GenderService {
   constructor(private httpClient: HttpClient) { }
 
   getGenderList(): Observable<Gender[]>{
-    return this.httpClient.get<Gender[]>(this.baseApiUrl + 'gender');
+    return this.httpClient.get<Gender[]>(this.baseApiUrl + '/genders');
   }
 }
